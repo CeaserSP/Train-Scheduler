f285f6008625342f3e26aecd18727f319bc3d2c2
Sheldon Perkins <csperkins18@gmail.com>
2019-09-02 12:57:38 -0400

    firebase to js

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB5O2scqj31yjfi1J0MArgjEg4nD9PP1Sc",
+    authDomain: "any-name-170a7.firebaseapp.com",
+    databaseURL: "https://any-name-170a7.firebaseio.com",
+    projectId: "any-name-170a7",
+    storageBucket: "",
+    messagingSenderId: "930142724357",
+    appId: "1:930142724357:web:95a4c163ed2dc252"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
