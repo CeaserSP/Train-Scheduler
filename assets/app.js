46af343373d94f26ba4c6205744b396272b5964f
Sheldon Perkins <csperkins18@gmail.com>
2019-09-03 18:40:37 -0400

    firebase link

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+// Your web app's Firebase configuration
+var firebaseConfig = {
     apiKey: "AIzaSyB5O2scqj31yjfi1J0MArgjEg4nD9PP1Sc",
     authDomain: "any-name-170a7.firebaseapp.com",
     databaseURL: "https://any-name-170a7.firebaseio.com",
@@ -7,6 +7,30 @@
     storageBucket: "",
     messagingSenderId: "930142724357",
     appId: "1:930142724357:web:95a4c163ed2dc252"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+// Click event
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
+    // Input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var trainTime = moment($("#train-input").val().trim(), "HH:mm").format("HH:mm");
+    var trainFrequency = $("#frequency-input").val().trim();
+
+    // Creates local "temporary" object for holding employee data
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency,
+    };
+    // push to the database
+    database.ref().push(newTrain);
+    console.log(newTrain);
+    // Clear
+    
+});
